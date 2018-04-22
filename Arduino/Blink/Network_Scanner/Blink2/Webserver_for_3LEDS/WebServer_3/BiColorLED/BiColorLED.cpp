81abbad83f7ceb3c2797756a15d4284eb25f408e
Tommy Long <lonto81@gmail.com>
2018-04-22 18:42:33 -0500

    Update BiColorLED.cpp

--- Arduino/Blink/Network_Scanner/Blink2/Webserver_for_3LEDS/WebServer_3/BiColorLED/BiColorLED.cpp
+++ Arduino/Blink/Network_Scanner/Blink2/Webserver_for_3LEDS/WebServer_3/BiColorLED/BiColorLED.cpp
@@ -1,5 +1,5 @@
 /*  Simple Bicolor LED library for Arduino, v1.2
-    Copyright (C) 2012 Wolfgang Faust
+   
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
     
-    Email: wolf@wolfgang.site40.net
+ 
 */
 #include "BiColorLED.h"
 
